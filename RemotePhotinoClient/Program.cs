9c97d78fd62d851ff86dc79ae106017eb5cceb7d
Bud Cribar <budcribar@msn.com>
2021-03-29 06:17:50 -0600

    save

--- RemotePhotinoClient/Program.cs
+++ RemotePhotinoClient/Program.cs
@@ -1,3 +1,5 @@
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,7 +19,22 @@ namespace RemotePhotinoClient
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+          
+            //Add gRPC service
+            builder.Services.AddSingleton(services =>
+            {
+                // Get the service address from appsettings.json
+                var config = services.GetRequiredService<IConfiguration>();
+                var backendUrl = "https://localhost:5001";
+
+                // Create a channel with a GrpcWebHandler that is addressed to the backend server.
+                //
+                // GrpcWebText is used because server streaming requires it. If server streaming is not used in your app
+                // then GrpcWeb is recommended because it produces smaller messages.
+                var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWebText, new HttpClientHandler());
+
+                return GrpcChannel.ForAddress(backendUrl, new GrpcChannelOptions { HttpHandler = httpHandler });
+            });
 
             builder.Services.AddMsalAuthentication(options =>
             {
